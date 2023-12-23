7d517630d016ab2eb837265e3573d6f2876656a6
captain-america-7 <ch.en.u4cys21046@ch.students.amrita.edu>
2023-12-23 11:56:44 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int m1, m2, m3 printf("Enter values of marks in 3 subjects") scanf("%d %d %d", &m1, &m2, &m3) printf("You entered %d %d %d", m1, m2, m3);
+}
+
+//Ans : We does not use ";" in 3, 4 and 5 th line.
