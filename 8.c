d6108f3c15f7f63a88c47f02ec889e33b5d4d5af
captain-america-7 <ch.en.u4cys21046@ch.students.amrita.edu>
2023-12-23 11:54:43 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,13 @@
+/*Calculate of average
+/* Author: Sanjay */
+/* Place - Whispering Bytes */
+*/
+#include <stdio.h>
+    int main()
+{
+    int a = 35;
+    float b = 3.24;
+    printf("%d %f %d", a, b + 1.5, 235);
+}
+// Error is before #include<stdio.h>.we can not write like this * / at the end.
+// Nested comments are not possible
