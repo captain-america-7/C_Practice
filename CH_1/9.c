420cb24a40e11b03ad38bc656523bd2ceb56c465
captain-america-7 <ch.en.u4cys21046@ch.students.amrita.edu>
2023-12-30 09:30:47 +0530

    Delete 9.c

--- CH_1/9.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a, b, c;
-    scanf("%d %d %d", a, b, c);
-}
-
-//In scanf before a, b and c we does not use '&'.
-//correct line will be : scanf(“% d % d % d”, &a, &b, &c);
