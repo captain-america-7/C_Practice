3e7dff6ae7ad48b74b57194ff632f47464b84f44
captain-america-7 <ch.en.u4cys21046@ch.students.amrita.edu>
2023-12-30 10:00:53 +0530

    Create 1_Profit_or_Loss.c

---
+++ CH_3/1_Profit_or_Loss.c
@@ -0,0 +1,24 @@
+/* Aruntej [21046] */
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    float cost_price, selling_price, loss, profit;
+
+    printf("\n Enter the cost price of an item=");
+    scanf("%f", &cost_price);
+
+    printf("\n Enter the selling price of an item=");
+    scanf("%f", &selling_price);
+    //Using if else for profit or loss calculation
+    if (cost_price > selling_price)
+    {
+        loss = cost_price - selling_price;
+        printf("\n The seller has suffered the loss.The loss is=%f", loss);
+    }
+    else
+    {
+        profit = selling_price - cost_price;
+        printf("\n The seller has gain the profit.The profit is=%f", profit);
+    }
+}
