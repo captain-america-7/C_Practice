a448bd5a8170d6258a36d19f4ed21f53da6759b7
captain-america-7 <ch.en.u4cys21046@ch.students.amrita.edu>
2023-12-30 09:30:51 +0530

    Delete 8.c

--- CH_1/8.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-    int main()
-{
-    int a = 35;
-    float b = 3.24;
-    printf("%d %f %d", a, b + 1.5, 235);
-}
-// Error is before #include<stdio.h>.we can not write like this * / at the end.
-// Nested comments are not possible
